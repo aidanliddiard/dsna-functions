5595efcf3c0621f606916ffff0698a90c055d896
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-22 10:10:44 -0700

    completed filter problem

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ function map(arr, callback) {
 }
 
 console.log(map(myArray, (n) => n * 2));
+
+
+function filter(arr, callback) {
+  const filteredArray = [];
+for (let i = 0; i<arr.length; i++) {
+  if(callback(arr[i])) {
+    const item = arr[i];
+    filteredArray.push(item);
+  }
+}
+  return filteredArray;
+}
+console.log(filter(myArray, n=> n %2 === 0 ));
