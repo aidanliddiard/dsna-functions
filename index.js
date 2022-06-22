b449491beb274824cd6684165eb3672021275f95
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-22 11:24:50 -0700

    first had working, last bit is not returning addUnsure

--- index.js
+++ index.js
@@ -51,3 +51,7 @@ const addExcitement = addPunctuation('!!!');
 
 console.log(addExcitement('Hello World'));
 console.log(addExcitement('Pokemon, catch em all'));
+const addUnsure = addPunctuation('?!?');
+console.log(addUnsure('Hello World'));
+// Hello World?!?
+console.log(addUnsure('Pokemon, catch em all'));
